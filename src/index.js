ad8bf147b2fe5e31048c6b438273fa617db1749f
ouegy <ianbutler82@yahoo.co.uk>
2024-10-15 16:24:49 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log("it works");
+
+fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/london?key=853CD7D6N7NPZHQML7FU2XQAL"
+)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (err) {});
