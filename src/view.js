69c59ea46abbfbdefa5886919c4beaa4d0f1b3a2
ouegy <ianbutler82@yahoo.co.uk>
2024-10-19 13:37:18 +0100

    Feature - week view, hourly view

--- src/view.js
+++ src/view.js
@@ -1,4 +1,5 @@
 import { weather } from "./weather";
+import moment from "moment";
 
 const view = (() => {
     function importAll(r) {
@@ -15,35 +16,103 @@ const view = (() => {
         let location = getLocation();
         const data = await weather.getLocationData(location);
         createTodayView(data);
-        createDaysView(data.days);
+        createWeekView(data.days, "7");
+        createHourlyView(data.days);
     }
     function getLocation() {
         let location = document.querySelector("input.location").value;
         return location;
     }
     function createTodayView(data) {
-        const title = document.querySelector("h1#title");
-        const temp = document.querySelector("h2#temp");
+        const title = document.querySelector("#title");
+        const date = document.querySelector("#date");
+        const temp = document.querySelector("#temp");
+        const max = document.querySelector("#max");
+        const min = document.querySelector("#min");
         const image = document.getElementById("icon");
+        const desc = document.getElementById("desc");
+        const conditions = document.getElementById("conditions");
+
         const icon = data.icon;
         const src = getImage(images, icon);
 
         //console.log(src);
+        date.textContent = "Today";
         title.textContent = data.address;
-        temp.textContent = data.temp + " °F";
+        temp.textContent = data.days[0].temp + " °F";
+        max.textContent = "Max: " + data.days[0].tempmax + " °F";
+        min.textContent = "Min: " + data.days[0].tempmin + " °F";
+        desc.textContent = data.desc;
+        conditions.textContent = data.conditions;
 
         image.setAttribute("src", src);
     }
-    function createDaysView(days) {
-        console.log(days);
+    function createWeekView(days, number) {
+        const result = days.slice(1, number);
+        console.log(result);
+        const week = document.querySelector("#week");
+        week.replaceChildren();
+
+        result.forEach((element) => {
+            const src = getImage(images, element.icon);
+            const div = document.createElement("div");
+            const day = document.createElement("span");
+            const date = document.createElement("span");
+            const image = document.createElement("img");
+            const temp = document.createElement("span");
+            image.setAttribute("src", src);
+            image.setAttribute("class", "icon");
+            div.setAttribute("class", "week");
+            day.textContent = getDay(element.datetime);
+            date.textContent = formatDate(element.datetime);
+            temp.textContent = element.temp + " °F";
+            div.appendChild(day);
+            div.appendChild(date);
+            div.appendChild(image);
+            div.appendChild(temp);
+            week.appendChild(div);
+        });
+        console.log(week);
+        return week;
     }
+    function createHourlyView(days) {
+        console.log(days);
+        let result = days[0]["hours"];
+        result = result.slice(6, 24);
+        console.log(result);
+        const hourly = document.querySelector("#hourly");
+        hourly.replaceChildren();
+
+        result.forEach((element) => {
+            const src = getImage(images, element.icon);
+            const div = document.createElement("div");
+            const hour = document.createElement("span");
+            const image = document.createElement("img");
+            const temp = document.createElement("span");
+            image.setAttribute("src", src);
+            image.setAttribute("class", "icon");
+            div.setAttribute("class", "week");
+            hour.textContent = element.datetime;
+            temp.textContent = element.temp + " °F";
+            div.appendChild(hour);
+            div.appendChild(image);
+            div.appendChild(temp);
+            hourly.appendChild(div);
+        });
+        return hourly;
+    }
+    function formatDate(date) {
+        const d = new Date(date);
+        return moment(d).format("MMM Do");
+    }
+    function getDay(date) {
+        const d = new Date(date);
+        return moment(d).format("dddd");
+    }
+    function getTime(day) {}
     function getImage(images, icon) {
-        console.log(images);
-        console.log(icon);
         const index = icon + ".svg"; // this not correct
         const image = images[index];
-        console.log(index);
-        console.log(image);
         return image;
     }
     function addGlobalEventListener(type, selector, callback, options) {
@@ -59,10 +128,17 @@ const view = (() => {
     return { buildViews, addGlobalEventListener };
 })();
 
-view.addGlobalEventListener("click", "#search", () => {
+view.addGlobalEventListener("click", "#search-button", (e) => {
+    e.preventDefault();
     let input = document.querySelector("input.location").value;
     console.log(input);
     view.buildViews();
+});
+
+view.addGlobalEventListener("keyup", "#search", (e) => {
+    let input = document.querySelector("input.location").value;
+    console.log(input);
+    if (e.keyCode === 13) view.buildViews();
 });
 
 export { view };
--- src/weather.js
+++ src/weather.js
@@ -15,9 +15,14 @@ const weather = (() => {
             function processData(json) {
                 const data = {
                     temp: json.currentConditions.temp,
+                    max: json.days[0].tempmax,
+                    min: json.days[0].tempmin,
                     address: json.resolvedAddress,
+                    desc: json.description,
                     icon: json.currentConditions.icon,
+                    conditions: json.currentConditions.conditions,
                     days: json.days,
+                    date: json.days[0].datetime,
                 };
                 return data;
             }
